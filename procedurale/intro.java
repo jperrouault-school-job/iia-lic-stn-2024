db4007372d05585b681e7c26bbe7827a7a75222e
Jeremy <jeremy.perrouault@ascadis.fr>
2024-10-14 10:38:20 +0200

    JOUR 1 - INTRO

---
+++ procedurale/intro.java
@@ -0,0 +1,8 @@
+
+// Une instruction : c'est un ordre qu'on va donner à notre programme
+// En Java, on termine une instruction par ";"
+// > SAUF quand vous avez une accolade (un bloc d'instructions)
+
+
+
+
