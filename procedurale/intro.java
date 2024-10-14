4922da4ea7dc4d8108de9b37287b451c0f6c6a97
Jeremy <jeremy.perrouault@ascadis.fr>
2024-10-14 10:38:56 +0200

    JOUR 1 - INTRO

--- procedurale/intro.java
+++ procedurale/intro.java
@@ -3,6 +3,35 @@
 // En Java, on termine une instruction par ";"
 // > SAUF quand vous avez une accolade (un bloc d'instructions)
 
+// Exemples d'instructions :
+// Conditions
+// > SI (if) ... SINON SI (else if) ... SINON (else)
+// > SWITCH
 
+// Types de variables
+// > Boolean, Integer, String, Float, Double, Long, Short, Charactere
+
+// Déclaration de variables
+// > Sachant que a est une variable, et b est une variable
+// > On déclare toujours une variable avec son Type puis son nom, éventuellement sa valeur
+// >> Integer a = 5;
+// >> Long maVariable = 8;
+
+// Réaffecter une valeur à une variable
+// > a = 7;
+
+// Imprimer un message (print, println)
+// Opérations maths
+// Comparaisons
+// > a == b
+// > a <= b
+// > a >= b
+// > a != b
+
+// Boucles
+// > while (TANT QUE .. FAIRE)
+// > do .. while (FAIRE .. TANT QUE)
+// > for (POUR)
+// > for (POUR CHAQUE)
 
 
