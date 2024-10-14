a57c17cc37176af72d6dc756a914d61ffef30e67
Jeremy <jeremy.perrouault@ascadis.fr>
2024-10-14 12:11:18 +0200

    JOUR 1 - Boucles, tableaux

--- procedurale/intro.java
+++ procedurale/intro.java
@@ -35,3 +35,54 @@
 // > for (POUR CHAQUE)
 
 
+
+
+
+// Boucles
+// While (TANT QUE ...)
+Integer a = 1;
+while (a <= 10) {
+    // Ici, un bloc d'instruction(s)
+    print(a);
+    // a = a + 1;
+    a++;
+}
+
+// Faire ... Tant que
+Integer a = 11;
+do {
+    // Ici, un bloc d'instruction(s)
+    print(a);
+    // a = a + 1;
+    a++;
+} while (a <= 10);
+
+// Pour
+for (Integer i = 0; i < 10; i++) {
+    print(i);
+}
+
+// Les chaines de caractères :
+String abc = "Entre guillemets";
+
+// Les caractères :
+Charactere c = 'c';
+
+// Les tableaux 1D :
+Integer[] monTab = new Integer[5];
+
+Integer[] monTab = { 4, 10, 5, 3, 7, 123 };
+
+print(monTab[1]);
+
+// Exercice : Afficher dans jshell chaque valeur du tableau
+
+
+// Les tableaux 2D :
+Integer[][] monTab = {
+    { 1, 2, 3 },
+    { 4, 5, 6 },
+    { 7, 8, 9 }
+};
+
+print(monTab[1][0]); // Affichera 4
