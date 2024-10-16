d2640cbdcd6072de03bb8033f96b97ac7ccbd7f1
Jeremy <jeremy.perrouault@ascadis.fr>
2024-10-16 10:02:23 +0200

    JOUR 3 - INTRO FONCTIONNELLE

---
+++ fonctionnelle/AppIntroFonctionnel.java
@@ -0,0 +1,49 @@
+package fonctionnelle;
+
+import java.util.function.BiFunction;
+
+public class AppIntroFonctionnel {
+    public static void main(String[] args) {
+        Maths maths = new Maths();
+
+        // Ca, c'est de la programmation orientée objet, classique ...
+        // On exécute additionner et on stock son résultat de retour
+        Integer resultat = maths.additionner(4, 5);
+
+        System.out.println(resultat);
+
+        // Pour passer sur de la programmation fonctionnelle, il faut utiliser des interfaces fonctionnelles
+        // > On peut les créer
+        // > On peut aussi utiliser celles déjà présentes dans Java
+
+        // Supplier -> Il retourne un résultat, et n'attend pas de paramètre
+        // Consumer -> Il retourne pas de résultat, et attend un paramètre
+        // Function -> Il retourne un résultat, et attend un paramètre
+
+        // On exécute PAS additionner, on stock simplement la référence à cette méthode
+        BiFunction<Integer, Integer, Integer> addFunction = maths::additionner;
+
+        // Et c'est ICI, qu'on exécute additionner et qu'on stock son résultat
+        resultat = addFunction.apply(8, 16);
+        
+        System.out.println(resultat);
+
+        demoFonction(maths::additionner);
+
+        BiFunction<Integer, Integer, Integer> minusFunction = (a, b) -> {
+            return a * b;
+        };
+        
+        demoFonction(minusFunction);
+    }
+
+    static void demoFonction(BiFunction<Integer, Integer, Integer> fonctionAExecuterPlusTard) {
+        // Du traitement blabla
+        Integer val1 = 8;
+        Integer val2 = 7;
+
+        Integer resultat = fonctionAExecuterPlusTard.apply(val1, val2);
+
+        System.out.println(resultat);
+    }
+}
---
+++ fonctionnelle/Maths.java
@@ -0,0 +1,7 @@
+package fonctionnelle;
+
+public class Maths {
+    public Integer additionner(Integer a, Integer b) {
+        return a + b;
+    }
+}
