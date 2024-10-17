f5b06aff333d66ee35075c770d7811cd19325f67
Jeremy <jeremy.perrouault@ascadis.fr>
2024-10-17 10:26:54 +0200

    JOUR 4 - LOGIQUE GRANDS PARENTS

--- logique/simpson.pl
+++ logique/simpson.pl
@@ -3,9 +3,16 @@ maman(marge, bart).
 maman(marge, lisa).
 maman(marge, maggie).
 
+maman(mona, homer).
+maman(jacqueline, marge).
+
 papa(homer, bart).
 papa(homer, lisa).
 papa(homer, maggie).
 
+papa(abraham, homer).
+papa(clancy, marge).
+
 % Règles
 parent(X, Y) :- maman(X, Y) ; papa(X, Y).
+grandparent(X, Z) :- parent(X, Y) , parent(Y, Z).
